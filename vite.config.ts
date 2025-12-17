c304a9aa9e9c28ab3e03b80ca559e0ca786aea8f
Barath-se <barathsiv2008@gmail.com>
2025-12-17 18:12:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/GD-Portfolio/', // 👈 VERY IMPORTANT (repo name)
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
